d6a7ca9517675c46b35451855e2eba37aaef5b22
Los-had <githubtestefinal@gmail.com>
2021-04-03 18:58:29 -0300

    2ª grande correção
    corrigi muita coisa e coloquei um sistema de nome

---
+++ docs/scripts/name.js
@@ -0,0 +1,10 @@
+var name = window.prompt('Qual é o seu nome/apelido ou nick?')
+document.writeln(`Seja bem-vindo ${name}`)
+
+if (name == null) {
+    document.writeln(`Você não digitou nenhum nome mais ok :)`)
+}
+else {
+    document.writeln('')
+}
+
